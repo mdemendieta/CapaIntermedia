9fcb6b746319e7357fbe96d884377171d2e91df7
VictorMolina15 <100103087+VictorMolina15@users.noreply.github.com>
2025-04-27 16:26:57 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -2,11 +2,12 @@
 // Parámetros de conexión
 $host = 'localhost'; // El host de la base de datos (puede ser localhost si está en el mismo servidor)
 $usuario = 'root';   // Nombre de usuario
-$contraseña = '1234'; // Contraseña
+$contraseña = ''; // Contraseña
 $base_de_datos = 'bd_capaInter'; // Nombre de la base de datos
+$puerto = '33065'; // Puerto de conexión (opcional, por defecto es 3306 para MySQL)
 
 // Crear la conexión
-$conn = new mysqli($host, $usuario, $contraseña, $base_de_datos);
+$conn = new mysqli($host, $usuario, $contraseña, $base_de_datos, $puerto);
 
 // Verificar la conexión
 if ($conn->connect_error) {
