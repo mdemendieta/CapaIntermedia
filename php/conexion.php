48f0ef556ab2fea1ccffd2a9ce5d71744e428d2f
mdemendieta <mdemendieta10@gmail.com>
2025-04-26 22:05:53 -0600

    conexion exitosa

---
+++ php/conexion.php
@@ -0,0 +1,20 @@
+<?php
+// Parámetros de conexión
+$host = 'localhost'; // El host de la base de datos (puede ser localhost si está en el mismo servidor)
+$usuario = 'root';   // Nombre de usuario
+$contraseña = '1234'; // Contraseña
+$base_de_datos = 'bd_capaInter'; // Nombre de la base de datos
+
+// Crear la conexión
+$conn = new mysqli($host, $usuario, $contraseña, $base_de_datos);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos '$base_de_datos'.";
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
